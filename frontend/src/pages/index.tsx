823f86b6891bae4c091eef05547b960f3fae4c3f
Jack <jckwind11@gmail.com>
2023-09-03 16:37:53 -0400

    seperated css to pages folder, added responsive background grad angle and colors

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,38 +1,14 @@
-import { GetServerSidePropsContext } from 'next';
 import '../app/globals.css';
-import Star, { StarProps } from '../component/star'
+import './css/index.css'
 
-export default function Home({ stars }: { stars: StarProps[] }) {
+export default function Home() {
 
   return (
-    <main className="relative min-h-screen p-24 bg-[#1a3139]">
-    {stars.map((star, index) => (
-        <Star key={index} {...star} />
-      ))}
+    <main>
+      <div className='h-screen w-screen custom-gradient'></div>
     </main>
   )
   
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const starCount = 500;
-  const gridSize = Math.sqrt(starCount);
-  const cellSize = 100 / gridSize;
-  const stars: StarProps[] = [];
-  const margin = 0.05 * cellSize;  // 5% margin within each cell
-
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      const x = i * cellSize + margin + Math.random() * (cellSize - 0.5 * margin);
-      const y = j * cellSize + margin + Math.random() * (cellSize - 0.5 * margin);
-      const size = (Math.random() * 0.5) + 0.3;
-      const duration = (Math.random() * 1) + 2;
-
-      stars.push({ x, y, size, duration });
-    }
-  }
-
-  return { props: { stars } };
 }
 
 
