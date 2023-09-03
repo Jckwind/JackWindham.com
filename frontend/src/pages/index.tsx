969f162c09e9ea2cd9370af0dd9902c6827453ec
Jack <jckwind11@gmail.com>
2023-09-03 17:12:20 -0400

    added meta data

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import '../app/globals.css';
 import './css/index.css'
+import Head from 'next/head';
 
 export default function Home() {
 
   return (
-    <main>
-      <div className='h-screen w-screen custom-gradient'></div>
-    </main>
+    <>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, viewport-fit=cover'></meta>
+      </Head>
+      <main>
+        <div className='h-screen w-screen custom-gradient'></div>
+      </main>
+    </>
   )
   
 }
