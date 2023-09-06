fd09d69092c8d301307896473dd8f4b3543b51c1
Jack <jckwind11@gmail.com>
2023-09-06 11:35:37 -0400

    trying this haha

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
     <>
       <Head>
         <meta name='viewport' content='initial-scale=1, viewport-fit=cover' />
-        <meta name="theme-color" content="var(--background-color)" />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#F1F0E8" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
       </Head>
     </>
   )
