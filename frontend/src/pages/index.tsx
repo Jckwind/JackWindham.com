d5544b4391f4a4cd41ae92f27d2b56f8a6643405
Jack <jckwind11@gmail.com>
2023-09-06 12:03:21 -0400

    switches background and secondary background for light

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <meta name='viewport' content='initial-scale=1, viewport-fit=cover' />
-        <meta name="theme-color" key="light" media="(prefers-color-scheme: light)" content="#E2C799" />
+        <meta name="theme-color" key="light" media="(prefers-color-scheme: light)" content="#F2ECBE" />
         <meta name="theme-color" key="dark" media="(prefers-color-scheme: dark)" content="#000000" />
       </Head>
     </>
