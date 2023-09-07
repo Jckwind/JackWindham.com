8f041a2bf01860ba47969e92cc56b71d8e73ec73
Jack <jckwind11@gmail.com>
2023-09-07 10:44:02 -0400

    added text effects to svg

--- frontend/src/component/VaraText.tsx
+++ frontend/src/component/VaraText.tsx
@@ -17,17 +17,29 @@ const VaraText: React.FC<VaraTextProps> = ({ text, fontSize = 32 }) => {
                 {
                 text,
                 fontSize,
-                strokeWidth: 1,
+                strokeWidth: 1.2,
                 letterSpacing: -14,
                 delay:500,
                 duration:2000,
                 textAlign:"center",
+                color: "#002b59"
                 }
             ]
         );
       }, [text, fontSize]);
     
-    return <div id={containerId}></div>;
+    return <div id={containerId}>
+        <svg>
+            <defs>
+                <filter id="noiseFilter1">
+                <feTurbulence baseFrequency="0.05" />
+                <feDisplacementMap in="SourceGraphic" scale="3" />
+                <feGaussianBlur stdDeviation="0.25" />
+                <feComposite operator="in" in="SourceGraphic" />
+                </filter>
+            </defs>
+        </svg>
+    </div>;
 };
 
 export default VaraText;
