fb1fb3340a01e01813f036855c09db178e52a543
Jack Wind <jckwind11@gmail.com>
2023-09-10 23:13:16 -0400

    added name (#5)
    inital UI!

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import VaraText from '@/component/VaraText';
+import { Laila } from 'next/font/google'
+import VaraText from '@/components/VaraText';
+import Typewriter from 'typewriter-effect';
+import Vara from 'vara'
 import Head from 'next/head';
+import Image from 'next/image';
 import './css/index.css'
 
+const laila = Laila({
+  subsets: ['latin'],
+  weight: '600',
+  display: 'swap',
+  variable: '--laila-font',
+})
+
+
 export default function Home() {
+
+  let varaInstance: InstanceType<typeof Vara>;
+
+  const onVaraInit = (instance: any) => {
+    varaInstance = instance;
+  };
 
   return (
     <>
@@ -13,11 +30,32 @@ export default function Home() {
         <meta name="theme-color" key="dark" media="(prefers-color-scheme: dark)" content="#000000" />
       </Head>
       <div className="textHolder">
-        <VaraText text="Jack" fontSize={75}/>
+        <h1 className={`${laila.className} welcome-label`}>Nouns. Adjectives. Things about me. Wow</h1>
+
+        {/* <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(1500)
+              .typeString('Welcome! My name is ')
+              .callFunction(() => {
+                varaInstance.draw("jack");
+              })
+              .start();
+          }}
+          options={{
+            cursor: "",
+            wrapperClassName: `${laila.className} welcome-label`,
+            delay: 80,
+          }}
+        /> */}
+        <div className="stickerHolder">
+          <Image src="mynameis.svg" alt="My Name Is" width={500} height={300} className='welcome-sticker' />
+          <VaraText text="Jack" fontSize={60} onVaraInit={onVaraInit} />
+        </div>
       </div>
     </>
   )
-  
+
 }
 
 
