ba90f68daf6555f3912335b5d0cbedfe4fd2ad5c
Jack <jckwind11@gmail.com>
2024-02-21 23:29:55 -0500

    changed about page to have placeholder

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -31,7 +31,7 @@ export default function About() {
         <Section>
           <h2 className="text-2xl font-bold">Coming soon</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            No, seriously. Like if you're reading this, it'll be updated soon.
+            No, seriously. Like if you&#39;re reading this, it&#39;ll be updated soon.
             Yes, like *soon* soon.  
           </p>
         </Section>
